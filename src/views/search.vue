a30c51f18088a8bf8c02e2b541416236e675fe08
dusk <exinyijiu@163.com>
2020-01-17 21:20:31 +0800

    布局OK

--- src/views/search.vue
+++ src/views/search.vue
@@ -3,8 +3,9 @@
   <!-- <Row >
         <Col span="24"> -->
         
-        <div type="flex" justify="center" align="middle" style="height:auto;padding-top:300px">
-          
+        <!-- <div type="flex" justify="center" align="middle" style="height:auto;padding-top:300px"> -->
+        <div class="content">
+        <div class="sea">
          <Input search enter-button="Search" v-model="keyword" style="width:500px;">
                     <Select v-model="select1" slot="prepend" style="width: 80px">
                        <Option value="http">http://</Option>
@@ -12,8 +13,10 @@
                    </Select>
                    
          </Input>
+         </div>
+         <div class="swi">
          <Switch v-model="switch1" @on-change="change" open="入库"/>
-          
+          </div>
           </div>
         
         <!-- </Col>
@@ -31,4 +34,27 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.content {
+  width: 700px;
+  margin: 350px auto;
+  /* background-color: red; */
+}
+.content:after {
+  clear:both;
+  content:'';
+  display:block;
+  width:0;
+  height:0;
+  visibility:hidden; 
+  }
+.sea {
+  margin-left: 80px;
+  float: left;
+}
+.swi {
+  margin-left: 5px;
+  padding-top: 5px;
+  float: left;
+}
+</style>
